c9a07fc0193e03c2105189418d97b8a11b9093aa
Seungil <seungilbang@khu.ac.kr>
2023-05-01 16:22:18 +0900

    [UPD] TodoRegisterController

--- src/main/java/com/webdev/servlet/controller/TodoRegisterController.java
+++ src/main/java/com/webdev/servlet/controller/TodoRegisterController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -24,6 +25,24 @@ public class TodoRegisterController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         log.info("/todo/register GET ..................................");
+
+        HttpSession session = request.getSession();
+
+        // 기존에 JSESSIONID 가 없는 새로운 사용자
+        if (session.isNew()) {
+            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자");
+            response.sendRedirect("/login");
+            return;
+        }
+
+        // JSESSIONID 는 있지만 해당 세션 컨텍스트에 loginInfo 라는 이름으로 저장된 객체가 없는 경우
+        if (session.getAttribute("loginInfo") == null) {
+            log.info("로그인한 정보가 없는 사용자");
+            response.sendRedirect("/login");
+            return;
+        }
+
+        // 로그인한 정보가 있는 정상적인 경우라면 등록 화면으로 이동
         request.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(request, response);
     }
 
